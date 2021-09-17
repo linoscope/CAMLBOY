93a8ae3da39f9f17f9bdeb839ad01beda77e3e1f
lin <linoshitani@gmail.com>
2021-09-18 02:27:56 +0900

    Refactor interrupt_controller

--- lib/interrupt_controller.ml
+++ lib/interrupt_controller.ml
@@ -1,28 +1,18 @@
 open Uints
 
-type state = {
-  mutable enabled : bool;
-  mutable requested : bool;
-}
-
-(* TODO: represent flags using bits like t = { ie : int; if_: int }  *)
 type t = {
-  vblank : state;
-  lcd_stat : state;
-  timer : state;
-  serial_port : state;
-  joypad : state;
-  ie_addr : Uint16.t;
-  if_addr : Uint16.t;
+  mutable ie: state;
+  mutable if_ : state;
 }
 
-let show t =
-  let open Bool in
-  Printf.sprintf "ie:(vblank:%d, lcd_stat:%d, timer:%d, serial_port:%d, joypad:%d), if:(vblank:%d, lcd_stat:%d, timer:%d, serial_port:%d, joypad:%d)"
-    (to_int t.vblank.enabled) (to_int t.lcd_stat.enabled) (to_int t.timer.enabled) (to_int t.serial_port.enabled) (to_int t.joypad.enabled)
-    (to_int t.vblank.requested) (to_int t.lcd_stat.requested) (to_int t.timer.requested) (to_int t.serial_port.requested) (to_int t.joypad.requested)
-
-let pp fmt t = Format.fprintf fmt "%s" (show t)
+and state = {
+  addr : uint16;
+  vblank : bool;
+  lcd_stat : bool;
+  timer : bool;
+  serial_port : bool;
+  joypad : bool;
+}
 
 type type_ =
   | VBlank
@@ -32,87 +22,93 @@ type type_ =
   | Joypad
 [@@deriving show]
 
+let show t =
+  let open Bool in
+  Printf.sprintf
+    "ie:(vblank:%d, lcd_stat:%d, timer:%d, serial_port:%d, joypad:%d), \
+     if:(vblank:%d, lcd_stat:%d, timer:%d, serial_port:%d, joypad:%d)"
+    (to_int t.ie.vblank) (to_int t.ie.lcd_stat)
+    (to_int t.ie.timer) (to_int t.ie.serial_port) (to_int t.ie.joypad)
+    (to_int t.if_.vblank) (to_int t.if_.lcd_stat)
+    (to_int t.if_.timer) (to_int t.if_.serial_port) (to_int t.if_.joypad)
+
+let pp fmt t = Format.fprintf fmt "%s" (show t)
+
 let create ~ie_addr ~if_addr = {
-  vblank = {enabled = true; requested = false};
-  lcd_stat = {enabled = true; requested = false};
-  timer = {enabled = true; requested = false};
-  serial_port = {enabled = true; requested = false};
-  joypad = {enabled = true; requested = false};
-  ie_addr;
-  if_addr;
+  ie  = {addr = ie_addr; vblank = true; lcd_stat = true; timer = true; serial_port = true; joypad = true};
+  if_ = {addr = if_addr; vblank = false; lcd_stat = false; timer = false; serial_port = false; joypad = false};
 }
 
-let state_of_type_ t = function
-  | VBlank -> t.vblank
-  | LCD_stat -> t.lcd_stat
-  | Timer -> t.timer
-  | Serial_port -> t.serial_port
-  | Joypad -> t.joypad
+let update state type_ b =
+  match type_ with
+  | VBlank      -> {state with vblank = b}
+  | LCD_stat    -> {state with lcd_stat = b}
+  | Timer       -> {state with timer = b}
+  | Serial_port -> {state with serial_port = b}
+  | Joypad      -> {state with joypad = b}
 
-let request t type_ =
-  let state = state_of_type_ t type_ in
-  state.requested <- true
 
-let clear t type_ =
-  let state = state_of_type_ t type_ in
-  state.requested <- false
+let request t type_ = t.if_ <- update t.if_ type_ true
+
+let clear t type_ = t.if_ <- update t.if_ type_ false
 
 let next t =
-  if t.vblank.enabled && t.vblank.requested then Some VBlank
-  else if t.lcd_stat.enabled && t.lcd_stat.requested then Some LCD_stat
-  else if t.timer.enabled && t.timer.requested then Some Timer
-  else if t.serial_port.enabled && t.serial_port.requested then Some Timer
-  else if t.joypad.enabled && t.joypad.requested then Some Joypad
-  else None
+  let get state = function
+    | VBlank      -> state.vblank
+    | LCD_stat    -> state.lcd_stat
+    | Timer       -> state.timer
+    | Serial_port -> state.serial_port
+    | Joypad      -> state.joypad
+  in
+  let check s1 s2 type_ = get s1 type_ && get s2 type_ in
+  [VBlank; LCD_stat; Timer; Serial_port; Joypad]
+  |> List.find_opt (check t.ie t.if_)
 
-let accepts t addr = Uint16.(t.ie_addr = addr || t.if_addr = addr)
+let accepts t addr = Uint16.(t.ie.addr = addr || t.if_.addr = addr)
 
-let byte_of_bools (b0, b1, b2, b3, b4) : uint8 =
-  (if b0 then 0b00001 else 0)
-  lor (if b1 then 0b00010 else 0)
-  lor (if b2 then 0b00100 else 0)
-  lor (if b3 then 0b01000 else 0)
-  lor (if b4 then 0b10000 else 0)
+let byte_of_state s =
+  (if s.vblank then 0b00001 else 0)
+  lor (if s.lcd_stat    then 0b00010 else 0)
+  lor (if s.timer       then 0b00100 else 0)
+  lor (if s.serial_port then 0b01000 else 0)
+  lor (if s.joypad      then 0b10000 else 0)
   |> Uint8.of_int
 
 let read_byte t addr =
-  if Uint16.(addr = t.ie_addr) then
-    byte_of_bools
-      (t.vblank.enabled, t.lcd_stat.enabled, t.timer.enabled, t.serial_port.enabled, t.joypad.enabled)
-  else if Uint16.(addr = t.if_addr) then
-    let open Uint8 in
+  if Uint16.(addr = t.ie.addr) then
+    byte_of_state t.ie
+  else if Uint16.(addr = t.if_.addr) then
     (* unused bits always return 1 *)
-    of_int 0b11100000 lor
-    (byte_of_bools
-       (t.vblank.requested, t.lcd_stat.requested, t.timer.requested, t.serial_port.requested, t.joypad.requested))
+    Uint8.(of_int 0b11100000 lor byte_of_state t.if_)
   else
     assert false
 
-let bools_of_byte (n : uint8) : bool * bool * bool * bool * bool =
-  let open Uint8 in
-  (
-    n land of_int 0b10000 <> of_int 0,
-    n land of_int 0b01000 <> of_int 0,
-    n land of_int 0b00100 <> of_int 0,
-    n land of_int 0b00010 <> of_int 0,
-    n land of_int 0b00001 <> of_int 0
-  )
+let state_of_byte b addr =
+  let bools_of_byte (n : uint8) : bool * bool * bool * bool * bool =
+    let open Uint8 in
+    (
+      n land of_int 0b10000 <> of_int 0,
+      n land of_int 0b01000 <> of_int 0,
+      n land of_int 0b00100 <> of_int 0,
+      n land of_int 0b00010 <> of_int 0,
+      n land of_int 0b00001 <> of_int 0
+    )
+  in
+  let (b4, b3, b2, b1, b0) = bools_of_byte b in
+  {
+    addr;
+    vblank = b0;
+    lcd_stat = b1;
+    timer = b2;
+    serial_port = b3;
+    joypad = b4;
+  }
 
 let write_byte t ~addr ~data =
-  let open Uint16 in
-  let (b4, b3, b2, b1, b0) = bools_of_byte data in
-  if addr = t.ie_addr then begin
-    t.vblank.enabled <- b0;
-    t.lcd_stat.enabled <- b1;
-    t.timer.enabled <- b2;
-    t.serial_port.enabled <- b3;
-    t.joypad.enabled <- b4
+  if addr = t.ie.addr then begin
+    t.ie <- state_of_byte data t.ie.addr
   end
-  else if addr = t.if_addr then begin
-    t.vblank.requested <- b0;
-    t.lcd_stat.requested <- b1;
-    t.timer.requested <- b2;
-    t.serial_port.requested <- b3;
-    t.joypad.requested <- b4;
+  else if addr = t.if_.addr then begin
+    t.if_ <- state_of_byte data t.if_.addr
   end else
     assert false
