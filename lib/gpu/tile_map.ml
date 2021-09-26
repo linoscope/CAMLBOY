3d2f1a6b61822954288e0248ceda6a44e6dc41f4
lin <linoshitani@gmail.com>
2021-09-26 10:16:10 +0900

    Implement tile_map

--- lib/gpu/tile_map.ml
+++ lib/gpu/tile_map.ml
@@ -1,3 +1,31 @@
+open Uints
+
+type t = {
+  ram : Ram.t;
+  area0_start_addr : uint16;
+  area1_start_addr : uint16;
+}
+
 type area =
   | Area0
   | Area1
+
+let create ~tile_map_ram ~area0_start_addr ~area1_start_addr = {
+  ram = tile_map_ram;
+  area0_start_addr;
+  area1_start_addr;
+}
+
+let get_tile t ~area ~i ~j =
+  let offset = match area with
+    | Area0 -> t.area0_start_addr
+    | Area1 -> t.area1_start_addr
+  in
+  let index = i * 32 + j |> Uint16.of_int in
+  Ram.read_byte t.ram Uint16.(offset + index) |> Uint8.to_int
+
+let accepts t = Ram.accepts t.ram
+
+let read_byte t = Ram.read_byte t.ram
+
+let write_byte t = Ram.write_byte t.ram
--- lib/gpu/tile_map.mli
+++ lib/gpu/tile_map.mli
@@ -1,3 +1,19 @@
+(** @see <https://gbdev.io/pandocs/Tile_Maps.html>  *)
+
+open Uints
+
+type t
+
 type area =
   | Area0
   | Area1
+
+val create :
+  tile_map_ram:Ram.t
+  -> area0_start_addr:uint16
+  -> area1_start_addr:uint16
+  -> t
+
+val get_tile : t -> area:area -> i:int -> j:int -> int
+
+include Addressable_intf.S with type t := t
