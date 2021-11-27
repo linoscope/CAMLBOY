b7505f29c1e0a11e5897f8e80e1c3f4c57f0b3c5
lin <linoshitani@gmail.com>
2021-11-27 16:55:20 +0900

    Properly use Fut.Syntax in web/bench.ml

--- bin/web/bench.ml
+++ bin/web/bench.ml
@@ -23,7 +23,7 @@ let run_rom_bytes rom_bytes frames =
   (Performance.now_ms G.performance) -. !start_time
 
 let run_rom_blob rom_blob frames =
-  let* result = Blob.array_buffer rom_blob in
+  let+ result = Blob.array_buffer rom_blob in
   match result with
   | Ok buf ->
     let rom_bytes =
@@ -32,10 +32,10 @@ let run_rom_blob rom_blob frames =
       (* Convert uint8 bigarray to char bigarray *)
       |> Obj.magic
     in
-    Fut.return @@ run_rom_bytes rom_bytes frames
+    run_rom_bytes rom_bytes frames
   | Error e ->
     Console.(log [Jv.Error.message e]);
-    Fut.return 0.
+    0.
 
 let run_rom_path rom_path frames =
   let* result = Fetch.url (Jstr.v rom_path) in
@@ -44,10 +44,15 @@ let run_rom_path rom_path frames =
     let body = Fetch.Response.as_body response in
     let* result = Fetch.Body.blob body in
     begin match result with
-      | Ok blob -> run_rom_blob blob frames
-      | Error e  -> Console.(log [Jv.Error.message e]); Fut.return 0.
+      | Ok blob ->
+        run_rom_blob blob frames
+      | Error e  ->
+        Console.(log [Jv.Error.message e]);
+        Fut.return 0.
     end
-  | Error e  -> Console.(log [Jv.Error.message e]); Fut.return 0.
+  | Error e  ->
+    Console.(log [Jv.Error.message e]);
+    Fut.return 0.
 
 let read_param param_key ~default =
   let uri = Window.location G.window in
@@ -60,20 +65,21 @@ let read_param param_key ~default =
   | Some jstr -> Jstr.to_string jstr
   | None -> default
 
-let () =
+let main () =
   (* Read URL parameters *)
   let rom_path = read_param "rom_path" ~default:"tobu.gb" in
   let frames = read_param "frames" ~default:"1500" |> int_of_string in
   (* Load initial rom *)
-  let fut = run_rom_path rom_path frames in
-  Fut.await fut (fun duration_ms ->
-      let duration = duration_ms /. 1000. in
-      let fps = Float.(of_int frames /. duration) in
-      let msg = Printf.sprintf "%8s: %s\n%8s: %d\n%8s: %f\n%8s: %f\n"
-          "ROM path" rom_path
-          "Frames" frames
-          "Duration" duration
-          "FPS" fps
-      in
-      let result_el = find_el_by_id "bench_result" in
-      El.set_children result_el [El.txt (Jstr.v msg)])
+  let+ duration_ms = run_rom_path rom_path frames in
+  let duration = duration_ms /. 1000. in
+  let fps = Float.(of_int frames /. duration) in
+  let msg = Printf.sprintf "%8s: %s\n%8s: %d\n%8s: %f\n%8s: %f\n"
+      "ROM path" rom_path
+      "Frames" frames
+      "Duration" duration
+      "FPS" fps
+  in
+  let result_el = find_el_by_id "bench_result" in
+  El.set_children result_el [El.txt (Jstr.v msg)]
+
+let () = Fut.await (main ()) Fun.id
